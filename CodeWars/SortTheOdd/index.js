d18154c6449e72ff16e2daa4b35441e2dfe06da8
Gabriel Achille <gabriel.achille13@gmail.com>
2026-09-11 22:01:14 -0400

    made changes

--- CodeWars/SortTheOdd/index.js
+++ CodeWars/SortTheOdd/index.js
@@ -3,27 +3,27 @@
 //while keeping the even numbers the same
 //6kyu
 
-function sortArray(array) {
-  let odd = array.filter((x) => x % 2 === 1).sort((a, b) => a - b);
+// function sortArray(array) {
+//   let odd = array.filter((x) => x % 2 === 1).sort((a, b) => a - b);
 
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] % 2 !== 0) {
-      array[i] = odd.shift();
-    }
-  }
-  return array;
-}
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] % 2 !== 0) {
+//       array[i] = odd.shift();
+//     }
+//   }
+//   return array;
+// }
 
 console.log(sortArray([7, 1]));
 console.log(sortArray([9, 8, 7, 6, 5, 4, 3, 2, 1, 0]));
 console.log(sortArray([1, 11, 2, 8, 3, 4, 5]));
 
-// function sortArray(arr) {
-//   let odd = arr.filter((x) => x % 2 === 1).sort((a, b) => a - b);
+function sortArray(arr) {
+  let odd = arr.filter((x) => x % 2 === 1).sort((a, b) => a - b);
 
-//   return arr.map((x) => (x % 2 === 1 ? odd.shift() : x));
-// }
+  return arr.map((x) => (x % 2 === 1 ? odd.shift() : x));
+}
 
-// function sortArray(arr) {
-//   let oddNum = arr.filter((x) => x % 2 !== 0);
-// }
+function sortArray(arr) {
+  let oddNum = arr.filter((x) => x % 2 !== 0);
+}
